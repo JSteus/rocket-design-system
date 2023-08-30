f8bb070f617a3340dd72aae4a8da9ecc77144c3a
JSteus <mateus2021r@outlook.com>
2023-08-30 16:57:40 -0300

    [commit inicial] core components

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,27 @@
+import { Avatar, AvatarProps } from '@rocket/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/jsteus.png',
+    alt: 'Mateus Carvalho',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+    alt: '',
+  },
+}
---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,24 @@
+import { Box, BoxProps, Text } from '@rocket/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: (
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        fringilla, quam imperdiet pretium congue, odio lacus iaculis dolor
+      </Text>
+    ),
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
